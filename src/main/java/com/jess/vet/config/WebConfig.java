d7f4e0956c297c7ea6217f38bccbdb4b64fac5a0
Jess <22jessicamarin@gmail.com>
2022-07-06 23:24:23 -0500

    Seeting up html from w3layouts

---
+++ src/main/java/com/jess/vet/config/WebConfig.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jess.vet.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ *
+ * @author jessica-22
+ */
+
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("(/|/index)").setViewName("index");
+        registry.addViewController("/about").setViewName("about");
+        registry.addViewController("/gallery").setViewName("gallery");
+        registry.addViewController("/services").setViewName("services");
+        registry.addViewController("/contact").setViewName("contact");
+    }
+    
+}
